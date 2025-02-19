d4529f636635585c609c8803749bab4cda8b3cef
Agostino Don <chad@PCAgo>
2025-02-19 18:05:16 +0100

    I tried

--- RGBMasterpiece/grid.cpp
+++ RGBMasterpiece/grid.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -11,13 +11,30 @@ int main() {
 
     int N, M, R, G, B;
     cin >> N >> M >> R >> G >> B;
-    
-    vector<string> grid(N, string(M, '_'));
+    int lim;
+    if((N & 1) == 0) lim = M * N/2;
+    else if((M & 1) == 0) lim = N * M/2;
+    else lim = N *((M+1)/2) - M/2;
+    int peak = max({R, B, G});
+    if(peak > lim){
+        cout << "NO" << endl;
+        return 0;
+    }
+    vector<string> grid(N, string(M, 'R'));
     bool ans = true;
-    
-    
-    
-    
+
+    vector<pair<int, char>> colors = {{R, 'R'}, {G, 'G'}, {B, 'B'}};
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            auto it = find_if(colors.begin(), colors.end(), [&](const pair<int, char>& p) {
+                return p.first == 0;
+            });
+            if (it != colors.end()) colors.erase(it);
+            grid[i][j] = colors[(i+j)%colors.size()].second;
+        }
+    }
     if (ans) {
         cout << "YES" << endl;
         for (int i = 0; i < N; ++i)
