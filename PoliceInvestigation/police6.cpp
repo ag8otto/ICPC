186565be3bd090f13716cdd1ad242a3094db71ea
ag8otto <agostino.don@gmail.com>
2025-03-13 22:47:04 +0100

    Soluzione forse POLIZIA?

--- PoliceInvestigation/police6.cpp
+++ PoliceInvestigation/police6.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <set>
 using namespace std;
-
-// input data
-
 int main()
 {
-    ifstream cin("input1.txt");
-    //  ofstream cout("output.txt");
+    ifstream cin("input0.txt");
+    //ofstream cout("output.txt");
+
     int N;
     long long M, L;
     vector<long long> D;
     cin >> N >> M >> L;
     D.resize(N);
+
     for (int i = 0; i < N; i++)
         cin >> D[i];
 
-    if (N == 1)
-    {
-        (D[0] + M > L && D[0] - M < 0) ? cout << 1 << endl : cout << 0 << endl;
-        return 0;
-    }
-    int min = N;
-    
-    
+    int min = -1;
+    if(M >= L) min = N;
+    if (N == 1){
+        (D[0] + M > L && D[0] - M < 0) ? min = 1 : min = 0;
+    }else{
+        if(D[0] > M || D[N-1] + M < L) min = 0;
+        if(D[0] == M || D[N-1] + M == L) min = 1;
+        else{
+            set<int> Delta_Spot;
+            for (int i = 0; i < N; i++){
+              (D[i] - M < 0) ? Delta_Spot.insert(0) : Delta_Spot.insert(D[i] - M);
+              (D[i] + M > L) ? Delta_Spot.insert(L) : Delta_Spot.insert(D[i] + M);  
+            }
+            
+        }
+    }   
     cout << min << endl;
     return 0;
 }
