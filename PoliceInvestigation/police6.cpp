f4cc5f9a74c7420146a6244cd32b3e4fdbd1fbc1
Agostino Don <agostino.don@edu.unito.it>
2025-03-15 10:57:24 +0100

    Quasi finito Police

--- PoliceInvestigation/police6.cpp
+++ PoliceInvestigation/police6.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <set>
+#include <map>
+#include <algorithm>
 using namespace std;
 int main()
 {
     ifstream cin("input0.txt");
-    //ofstream cout("output.txt");
+    // ofstream cout("output.txt");
 
     int N;
     long long M, L;
@@ -17,22 +18,41 @@ int main()
     for (int i = 0; i < N; i++)
         cin >> D[i];
 
-    int min = -1;
-    if(M >= L) min = N;
-    if (N == 1){
-        (D[0] + M > L && D[0] - M < 0) ? min = 1 : min = 0;
-    }else{
-        if(D[0] > M || D[N-1] + M < L) min = 0;
-        if(D[0] == M || D[N-1] + M == L) min = 1;
-        else{
-            set<int> Delta_Spot;
-            for (int i = 0; i < N; i++){
-              (D[i] - M < 0) ? Delta_Spot.insert(0) : Delta_Spot.insert(D[i] - M);
-              (D[i] + M > L) ? Delta_Spot.insert(L) : Delta_Spot.insert(D[i] + M);  
+    int checkspot = -1;
+    if (M >= L)
+        checkspot = N;
+    else if (N == 1)
+        (D[0] + M > L && D[0] - M < 0) ? checkspot = 1 : checkspot = 0;
+    else
+    {
+        if (D[0] > M || D[N - 1] + M < L)
+            checkspot = 0;
+        else if (D[0] == M || D[N - 1] + M == L)
+            checkspot = 1;
+        else
+        {
+            map<long long, int> spot;
+            for (int i = 0; i < N; i++)
+            {
+                long long start = max(0LL, D[i] - M);
+                long long end = D[i] + M;
+                spot[start]++;
+                if (end + 1 < L)
+                    spot[end + 1]--;
             }
-            
+            for (const auto &p : spot)
+            {
+                cout << "(" << p.first << ", " << p.second << ")" << endl;
+            }
+            checkspot = N;
+            int currcheck = 0;
+            for (auto &event : spot)
+            {
+                currcheck += event.second;
+                checkspot = min(checkspot, currcheck);
+            }
         }
-    }   
-    cout << min << endl;
+    }
+    cout << checkspot << endl;
     return 0;
 }
